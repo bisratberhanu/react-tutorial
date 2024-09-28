2253c1ad400ad2a5ef7a48a7b7e8774651b6ef7a
bisratberhanu <bisratbnegus@gmail.com>
2024-09-28 12:38:07 +0300

    accordion complete

--- 04-basic Projects/04-Accordion/src/Question.js
+++ 04-basic Projects/04-Accordion/src/Question.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ id, title, info }) => {
+  const [more, setMore] = useState(false);
+  if (more) {
+    return (
+      <div className="question">
+        <header>
+          <h4> {title}</h4>
+          <button className="btn" onClick={() => setMore(!more)}>
+            {" "}
+            <AiOutlineMinus />
+          </button>
+        </header>
+        <p>{info}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="question">
+        <header>
+
+        <h4> {title}</h4>
+        <button className="btn" onClick={()=> setMore(!more)}>
+          <AiOutlinePlus></AiOutlinePlus>
+        </button>
+        </header>
+      </div>
+    );
+  }
 };
 
 export default Question;
